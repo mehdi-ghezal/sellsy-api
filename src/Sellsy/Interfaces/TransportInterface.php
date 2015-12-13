ee2a16444ba0f4437ea2c8decc49bd68dbb23e8a
Mehdi Ghezal <mehdi.ghezal@gmail.com>
2015-12-13 11:04:43 +0100

    Add a new transport class

---
+++ src/Sellsy/Interfaces/TransportInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Sellsy\Interfaces;
+
+/**
+ * Interface TransportInterface
+ * @package Sellsy\Interfaces
+ */
+interface TransportInterface
+{
+    /**
+     * @var string
+     */
+    const API_ENDPOINT = "https://apifeed.sellsy.com/0/";
+
+    /**
+     * @param array $requestSettings
+     * @return mixed
+     * @throws \Sellsy\Exception\ServerException
+     */
+    public function call(array $requestSettings);
+}
